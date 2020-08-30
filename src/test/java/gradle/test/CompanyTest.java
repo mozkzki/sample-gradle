5784366446fea87498686a4719b7af203e19e254
yukkun007 <kato.yutaka@gmail.com>
2020-08-31 06:28:07 +0900

    add

--- src/test/java/gradle/test/CompanyTest.java
+++ src/test/java/gradle/test/CompanyTest.java
@@ -9,9 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
 
 @ExtendWith(MockitoExtension.class)
 public class CompanyTest {
@@ -26,14 +23,6 @@ public class CompanyTest {
     public void testGreeting() {
         when(user.getGreeting()).thenReturn("user_x");
         assertEquals("Hoge: user_x", company.getGreeting());
-    }
-
-    @Test
-    private void test() {
-        String queueName = "hoge";
-        SqsClient sqsClient = SqsClient.builder().region(Region.US_WEST_2).build();
-        CreateQueueRequest createQueueRequest = CreateQueueRequest.builder().queueName(queueName).build();
-        sqsClient.createQueue(createQueueRequest);
     }
 
 }
