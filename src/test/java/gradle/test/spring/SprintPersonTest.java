1214ddabe5c70c7ecd6f130ea5fc060c60aed31a
yukkun007 <kato.yutaka@gmail.com>
2020-09-09 06:34:09 +0900

    add

--- src/test/java/gradle/test/spring/SprintPersonTest.java
+++ src/test/java/gradle/test/spring/SprintPersonTest.java
@@ -2,7 +2,7 @@ package gradle.test.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig(classes = TestConfig.class)
 public class SprintPersonTest {
