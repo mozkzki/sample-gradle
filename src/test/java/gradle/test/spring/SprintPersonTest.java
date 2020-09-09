9ad39a8b3c586299b0b5dabfd18d10b121ce1815
yukkun007 <kato.yutaka@gmail.com>
2020-09-09 16:58:33 +0900

    add

--- src/test/java/gradle/test/spring/SprintPersonTest.java
+++ src/test/java/gradle/test/spring/SprintPersonTest.java
@@ -2,19 +2,28 @@ package gradle.test.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig(classes = TestConfig.class)
 public class SprintPersonTest {
-    // @Autowired
-    // ApplicationContext ctx;
-
     @Autowired
-    SpringPerson person;
+    ApplicationContext ctx;
+
+    // 引数付きコンストラクタしかないので@Autowiredが使えない
+    // @Autowired
+    // SpringPerson person;
+
+    @Value("${test_param:foo}")
+    private String testParam;
 
     @Test
     void testGetGreeting() {
-        // SpringPerson person = ctx.getBean(SpringPerson.class, "aaaaaaa");
+        // 引数付きコンストラクタしかないのでgetBean
+        SpringPerson person = ctx.getBean(SpringPerson.class, "aaaaaaa");
+
+        System.out.println(testParam);
         person.getGreeting();
     }
 
